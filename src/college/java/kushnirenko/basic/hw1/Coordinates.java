01df22ae93c3e4bb4299b74c6ba5bc4d5b143d5e
KushnirenkoPetya <messsi20008@gmail.com>
2025-10-19 23:30:37 +0300

    test1

---
+++ src/college/java/kushnirenko/basic/hw1/Coordinates.java
@@ -0,0 +1,13 @@
+package college.java.kushnirenko.basic.hw1;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        double latitude = 0.0;
+        double longitude = 0.0;
+        System.out.println(" ");
+        System.out.println("Welcome: ");
+        System.out.println(" ");
+        System.out.println("latitude " + latitude);
+        System.out.println("longitude " + longitude);
+    }
+}
---
+++ src/college/java/kushnirenko/basic/hw2/Parallelepiped.java
@@ -0,0 +1,24 @@
+package college.java.kushnirenko.basic.hw2;
+
+public class Parallelepiped {
+    public static void main(String[] args){
+        int length = 1;
+        int width = 1;
+        int height = 1;
+        int volume = 0;
+        int lenghtAllSide = 0;
+        volume = length * width * height;
+        lenghtAllSide = (length + width + height);
+        System.out.println(" ");
+        System.out.println("start to program: ");
+        System.out.println("volume= " + volume);
+        System.out.println("lenghtAllSide= " + lenghtAllSide);
+
+
+
+
+
+
+    }
+
+}
