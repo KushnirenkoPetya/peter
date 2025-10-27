33b51799646f655d3a22cd39a8c6ea29db1feb95
KushnirenkoPetya <messsi20008@gmail.com>
2025-10-27 21:21:02 +0200

    PracticalTwo

---
+++ src/practical2/Lab2KushnirenkoKn21.java
@@ -0,0 +1,24 @@
+package practical2;
+
+public class Lab2KushnirenkoKn21 {
+    public static void main(String[] args) {
+        System.out.println("Information about primitive data types:");
+        System.out.println("");
+        System.out.println("byte   - size: " + Byte.BYTES + " bytes, min: " + Byte.MIN_VALUE +
+                ", max: " + Byte.MAX_VALUE);
+        System.out.println("short  - size: " + Short.BYTES + " bytes, min: " + Short.MIN_VALUE +
+                ", max: " + Short.MAX_VALUE);
+        System.out.println("int    - size: " + Integer.BYTES + " bytes, min: " + Integer.MIN_VALUE +
+                ", max: " + Integer.MAX_VALUE);
+        System.out.println("long   - size: " + Long.BYTES + " bytes, min: " + Long.MIN_VALUE +
+                ", max: " + Long.MAX_VALUE);
+        System.out.println("float  - size: " + Float.BYTES + " bytes, min: " + Float.MIN_VALUE +
+                ", max: " + Float.MAX_VALUE);
+        System.out.println("double - size: " + Double.BYTES + " bytes, min: " + Double.MIN_VALUE +
+                ", max: " + Double.MAX_VALUE);
+        System.out.println("char   - size: " + Character.BYTES + " bytes, min: " + (int) Character.MIN_VALUE +
+                ", max: " + (int) Character.MAX_VALUE);
+        System.out.println("boolean - possible values: true or false");
+
+    }
+}
