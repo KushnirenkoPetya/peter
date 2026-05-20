3cc95caf06b565b3934565e0b2904ba0fa80ee81
KushnirenkoPetya <messsi20008@gmail.com>
2026-05-20 20:29:24 +0300

    Practical

--- src/college/java/kushnirenko/basic/practical17/MyList.java
+++ src/college/java/kushnirenko/basic/practical17/MyList.java
@@ -1,119 +1,120 @@
 package college.java.kushnirenko.basic.practical17;
 
 public class MyList {
+
     private String[] elementData;
-    private int size = 0;
+    private int size;
 
-    public class MyList {
+    public MyList() {
+        elementData = new String[10];
+        size = 0;
+    }
 
-        private String[] elementData;
-        private int size;
+    public void add(String element) {
 
-        public MyList() {
-            elementData = new String[10];
-            size = 0;
-        }
+        ensureCapacity();
 
-        // Додавання в кінець
-        public void add(String element) {
-            ensureCapacity();
+        elementData[size] = element;
+        size++;
+    }
 
-            elementData[size] = element;
-            size++;
-        }
+    public void add(int index, String element) {
 
-        // Додавання за індексом
-        public void add(int index, String element) {
-            checkPositionIndex(index);
+        checkPositionIndex(index);
 
-            ensureCapacity();
+        ensureCapacity();
+
+        System.arraycopy(
+                elementData,
+                index,
+                elementData,
+                index + 1,
+                size - index
+        );
+
+        elementData[index] = element;
+
+        size++;
+    }
+
+    public void remove(int index) {
+
+        checkElementIndex(index);
+
+        int moved = size - index - 1;
+
+        if (moved > 0) {
 
             System.arraycopy(
-                    elementData,
-                    index,
                     elementData,
                     index + 1,
-                    size - index
-            );
-
-            elementData[index] = element;
-            size++;
-        }
-
-        // Видалення за індексом
-        public void remove(int index) {
-            checkElementIndex(index);
-
-            int moved = size - index - 1;
-
-            if (moved > 0) {
-                System.arraycopy(
-                        elementData,
-                        index + 1,
-                        elementData,
-                        index,
-                        moved
-                );
-            }
-
-            size--;
-            elementData[size] = null;
-        }
-
-        // Отримання елемента
-        public String get(int index) {
-            checkElementIndex(index);
-
-            return elementData[index];
-        }
-
-        // Кількість елементів
-        public int getSize() {
-            return size;
-        }
-
-        // Розмір буфера
-        public int getCapacity() {
-            return elementData.length;
-        }
-
-        // Збільшення буфера
-        private void ensureCapacity() {
-            if (size == elementData.length) {
-                grow();
-            }
-        }
-
-        private void grow() {
-            int newCapacity = elementData.length * 2;
-
-            String[] newArray = new String[newCapacity];
-
-            System.arraycopy(
                     elementData,
-                    0,
-                    newArray,
-                    0,
-                    elementData.length
+                    index,
+                    moved
             );
-
-            elementData = newArray;
         }
 
-        // Перевірка індекса
-        private void checkElementIndex(int index) {
-            if (index < 0 || index >= size) {
-                throw new IndexOutOfBoundsException(
-                        "Невірний індекс: " + index
-                );
-            }
-        }
+        size--;
 
-        private void checkPositionIndex(int index) {
-            if (index < 0 || index > size) {
-                throw new IndexOutOfBoundsException(
-                        "Невірний індекс: " + index
-                );
-            }
+        elementData[size] = null;
+    }
+
+    public String get(int index) {
+
+        checkElementIndex(index);
+
+        return elementData[index];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return elementData.length;
+    }
+
+    private void ensureCapacity() {
+
+        if (size == elementData.length) {
+            grow();
         }
     }
+
+    private void grow() {
+
+        int newCapacity = elementData.length * 2;
+
+        String[] newArray = new String[newCapacity];
+
+        System.arraycopy(
+                elementData,
+                0,
+                newArray,
+                0,
+                elementData.length
+        );
+
+        elementData = newArray;
+    }
+
+    private void checkElementIndex(int index) {
+
+        if (index < 0 || index >= size) {
+
+            throw new IndexOutOfBoundsException(
+                    "неверный индекс: " + index
+            );
+        }
+    }
+
+    private void checkPositionIndex(int index) {
+
+        if (index < 0 || index > size) {
+
+            throw new IndexOutOfBoundsException(
+                    "неверный индекс: " + index
+            );
+        }
+    }
+}
